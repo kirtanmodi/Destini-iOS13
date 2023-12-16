456483c8e09d9f632ff6eb22907404f4b923e7c7
Kirtan Modi <modikirtan19@gmail.com>
2023-12-16 11:48:25 -0600

    Update ViewController.swift with new UI elements and functions

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,18 +1,18 @@
-// Importing UIKit framework
+
 import UIKit
 
-// Defining a ViewController class that inherits from UIViewController
+
 class ViewController: UIViewController {
 
-    // IBOutlet properties for UI elements
-    @IBOutlet weak var storyLabel: UILabel!
-    @IBOutlet weak var choice1Button: UIButton!
-    @IBOutlet weak var choice2Button: UIButton!
     
-    // Variable to keep track of user's position in the story
+    @IBOutlet weak var storyLabel: UILabel! 
+    @IBOutlet weak var choice1Button: UIButton! 
+    @IBOutlet weak var choice2Button: UIButton! 
+    
+    
     var userPosition: Int = 0
     
-    // Array of Story objects
+    
     let story = [
         Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
         Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
@@ -21,8 +21,42 @@ class ViewController: UIViewController {
         Story(title: "You come across a river.", choice1: "Build a raft.", choice2: "Swim across."),
         Story(title: "You see a castle in the distance.", choice1: "Approach the castle.", choice2: "Continue on your path.")
     ]
+
     
-    // Dictionary to map user positions to next steps
+    func showToast(message : String) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+
+      func resetGame() {
+        userPosition = 0 
+        updateUI() 
+    }
+
+    func gameOver() {
+        let gameOverVC = GameOverViewController()
+        gameOverVC.modalPresentationStyle = .fullScreen
+        gameOverVC.dismissalHandler = { [weak self] in
+            self?.resetGame()
+        }
+        self.present(gameOverVC, animated: true, completion: nil)
+    }
+
+    
+    
     var storyMap: [Int: [Int]] = [
         0: [1, 2],
         1: [3, 4],
@@ -30,55 +64,86 @@ class ViewController: UIViewController {
         3: [7, 8],
         4: [9, 10],
         5: [11, 12]
-        // Add more mappings as needed
+        
     ]
 
-    // Function to update the UI based on the user's position
+    
     func updateUI() {
-        // Check if userPosition is within the bounds of the story array
-        guard userPosition < story.count else {
-            // Show an error or a default story
+        
+           guard userPosition < story.count else {
+            showToast(message: "Game Over")
+            gameOver() 
             return
         }
         
-        // Get the current story based on userPosition
+        
         let currentStory = story[userPosition]
         storyLabel.text = currentStory.title
         
-        // Set the title of choice1Button if it is not empty
+        
         if !currentStory.choice1.isEmpty {
             choice1Button.setTitle(currentStory.choice1, for: .normal)
         }
         
-        // Set the title of choice2Button if it is not empty
+        
         if !currentStory.choice2.isEmpty {
             choice2Button.setTitle(currentStory.choice2, for: .normal)
         }
     }
 
-    // Action method called when a choice button is tapped
+    
     @IBAction func choiceMade(_ sender: UIButton) {
-        // Get the next steps based on userPosition from the storyMap dictionary
-        guard let nextSteps = storyMap[userPosition] else {
-            // Show an error or a default story
+        
+        guard let nextSteps:[Int] = storyMap[userPosition] else {
+            
             return
         }
         
-        // Update userPosition based on the button tapped
+        
         if sender == choice1Button {
             userPosition = nextSteps[0]
         } else {
             userPosition = nextSteps[1]
         }
 
-        // Update the UI based on the new userPosition
+        
         updateUI()
     }
     
-    // Called after the controller's view is loaded into memory
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Update the initial UI
+        
         updateUI()
     }
+}
+
+class GameOverViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        
+        let startAgainButton = UIButton(type: .system)
+        startAgainButton.setTitle("Start Again", for: .normal)
+        startAgainButton.addTarget(self, action: #selector(startAgainTapped), for: .touchUpInside)
+        
+        
+        startAgainButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(startAgainButton)
+        NSLayoutConstraint.activate([
+            startAgainButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startAgainButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            startAgainButton.widthAnchor.constraint(equalToConstant: 200),
+            startAgainButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    var dismissalHandler: (() -> Void)?
+
+    @objc func startAgainTapped() {
+        dismiss(animated: true) {
+            self.dismissalHandler?()
+        }
+    }
+    
 }
