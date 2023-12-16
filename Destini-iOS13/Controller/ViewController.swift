1e861b9db0c63a33158b8ba839332b898e035263
Kirtan Modi <modikirtan19@gmail.com>
2023-12-16 11:14:35 -0600

    Add new stories to the storyline

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -11,14 +11,18 @@ class ViewController: UIViewController {
     let story = [
         Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
         Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-        Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps.")
+        Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps."),
+        Story(title: "You encounter a friendly wizard.", choice1: "Ask for a spell.", choice2: "Ignore him."),
+        Story(title: "You come across a river.", choice1: "Build a raft.", choice2: "Swim across."),
+        Story(title: "You see a castle in the distance.", choice1: "Approach the castle.", choice2: "Continue on your path.")
     ]
-
-    // New: Map to manage the storyline based on choices
     var storyMap: [Int: [Int]] = [
-        0: [1, 2], // From first story, left goes to 1, right goes to 2
-        1: [3, 4], // Additional stories can be added here
-        2: [5, 6], // And here
+        0: [1, 2],
+        1: [3, 4],
+        2: [5, 6],
+        3: [7, 8],
+        4: [9, 10],
+        5: [11, 12]
         // Add more mappings as needed
     ]
 
